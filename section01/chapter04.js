02e1fe7d1133ec52611e59d2d5c8f8db6f551bea
Kim-jong-yeop <114080381+kjy0124@users.noreply.github.com>
2025-02-02 14:05:39 +0900

    cp 03~05

---
+++ section01/chapter04.js
@@ -0,0 +1,6 @@
+//변수
+let age = 26;
+console.log(age);
+
+//상수 : 변하지 않는 값일 경우 사용
+const birth = "2000.01.24";
---
+++ section01/chapter05.js
@@ -0,0 +1,28 @@
+// 1. Number Type
+let num1 = 26;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+// 2. String Type
+let myName = "김종엽";
+let myLocation = "대구";
+let introduce = myName + myLocation;
+
+//템플릿 리터럴 문법
+let introduceText = `${myName}님은 ${myLocation}에 거주하고 있습니다.`;
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+//4. Nall Type : 아무것도 없다
+let empty = null;
+
+// 5. Undefined Type
+let none;
+console.log(none);
