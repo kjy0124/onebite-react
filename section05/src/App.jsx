3458040d7fd4e3a9dc9f0cb6334ea51b58aab944
kjy0124 <0124kimjy@naver.com>
2025-02-08 19:34:32 +0900

    ✨ React 컴포넌트 구조 이해해
    React Components

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <h1>안녕</h1>
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
