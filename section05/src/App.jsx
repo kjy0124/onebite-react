fd5ad2390c41d8a5fa2356d2be6759494ca812b9
kjy0124 <0124kimjy@naver.com>
2025-02-09 15:52:51 +0900

    ✨ State Practice
    State, setState

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,24 +1,51 @@
 import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-import Button from "./components/Button";
+// import Header from "./components/Header";
+// import Main from "./components/Main";
+// import Footer from "./components/Footer";
+// import Button from "./components/Button";
+
+//state
+import { useState } from "react";
 
 function App() {
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
+  //state(count)에는 위의 useState를 통해 생성한 state(count)의 값
+  //setState(setCount)에는 state(count)의 값을 변경시키는 함수
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState("OFF");
+  // const buttonProps = {
+  //   text: "메일",
+  //   color: "red",
+  //   a: 1,
+  //   b: 2,
+  //   c: 3,
+  // };
   return (
     <>
-      <Button {...buttonProps} />
+      <div>
+        <h1>{light}</h1>
+        <button
+          onClick={() => {
+            setLight(light === "ON" ? "OFF" : "ON");
+          }}
+        >
+          {light === "ON" ? "끄기" : "켜기"}
+        </button>
+      </div>
+      <div>
+        <h1>{count}</h1>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
+      {/* <Button {...buttonProps} />
       <Button text={"카페"} />
       <Button text={"블로그"}>
-        <Header />
-      </Button>
+        <Header / >
+      </Button> */}
     </>
   );
 }
