81390ea8336eeb89108fe921d85dca15c3cc051c
kjy0124 <0124kimjy@naver.com>
2025-02-09 15:13:48 +0900

    ✨ EventHandling Practice
    EventHandling

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,7 +1,16 @@
 const Button = ({ text, color, children }) => {
   const safeColor = color || "black"; // undefined일 경우 기본값 적용
+  //이벤트 객체
+  const onClickButton = (e) => {
+    console.log(e);
+    console.log(text);
+  };
   return (
-    <button style={{ color: safeColor }}>
+    <button
+      onClick={onClickButton}
+      // onMouseEnter={onClickButton} //마우스 커서 올렸을 때 동작
+      style={{ color: safeColor }}
+    >
       {text} - {safeColor.toUpperCase()}
       {children}
     </button>
