c26d9e69a9cb1d5018cb58b6478aee3f68f64fcf
kjy0124 <0124kimjy@naver.com>
2025-04-04 00:14:21 +0900

    📝 Add components

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -1,7 +1,15 @@
+import Controller from "./components/Controller";
+import Viewer from "./components/Viewer";
 import "./App.css";
 
 function App() {
-  return <>카운터 앱</>;
+  return (
+    <>
+      카운터 앱
+      <Viewer />
+      <Controller />
+    </>
+  );
 }
 
 export default App;
