ecaf915bdf9e8cf55e77819bf30b8d38bbaff592
kjy0124 <0124kimjy@naver.com>
2025-04-05 16:17:10 +0900

    📝 UseEffect 공부

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Controller from "./components/Controller";
 import Viewer from "./components/Viewer";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    console.log(`${count} / ${input}`);
+  }, [count, input]);
 
   const onClickBtn = (value) => {
     setCount(count + value);
@@ -13,6 +18,13 @@ function App() {
   return (
     <div className="App">
       <h1>Simple Counter</h1>
+      <section>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </section>
       <section>
         <Viewer count={count} />
       </section>
