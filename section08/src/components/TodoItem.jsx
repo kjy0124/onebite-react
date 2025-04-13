f22a93f43bc1bc4c993c68cf35e6863e15461633
kjy0124 <0124kimjy@naver.com>
2025-04-14 00:26:52 +0900

    ✨ Update Checkbox

--- section08/src/components/TodoItem.jsx
+++ section08/src/components/TodoItem.jsx
@@ -1,9 +1,18 @@
 import "./TodoItem.css";
 
-const TodoItem = ({ id, isDone, content, date }) => {
+const TodoItem = ({ id, isComplete, content, date, onUpdate }) => {
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  };
+
   return (
     <div className="TodoItem">
-      <input key={id} checked={isDone} type="checkbox" />
+      <input
+        key={id}
+        checked={isComplete}
+        type="checkbox"
+        onChange={onChangeCheckbox}
+      />
       <div className="content">{content}</div>
       <div className="date">{new Date(date).toLocaleDateString()}</div>
       <button>삭제</button>
